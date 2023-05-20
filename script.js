f28ac08f2e4d06cf92788fc0706be0034f8f1f40
Alfredo Damian Palacios <adpdamy0alf@gmail.com>
2023-05-20 13:54:10 -0300

    Arreglado nav-Registro, Optimizado footer, Terminadas las secciones reseña(API) y reserva, cambios y adiciones a los archivos css y js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* INICIO-CARRUSEL */
+// INICIO-CARRUSEL 
 const carrusel = document.querySelector(".carrusel")
 firstImg = document.querySelectorAll(".carrusel img")[0]
 arrowIcons = document.querySelectorAll(".carrConteiner i")
@@ -40,4 +40,39 @@ carrusel.addEventListener("mousedown", dragStart)
 carrusel.addEventListener("mousemove", dragging)
 carrusel.addEventListener("mouseup", dragStop)
 
-/* fin INICIO-CARRUSEL */
+// fin INICIO-CARRUSEL 
+
+// RESEÑAS 
+
+const {createApp} = Vue
+const miAplicacion = createApp({
+    // DATOS de la aplicacion
+    data() {
+        return { HTML_a_mostrar: ""}
+    },
+
+    methods: {
+        // Esta función lee los datos remotos, usando fetch
+        traerDatosAPI() {
+            fetch('https://randomuser.me/api') // API a leer
+                // Cuando ha finalizado la lectura
+                // guardo en datos el texto leido:
+                .then(datos => datos.json()) //res guarda el dato mediante el método .json()
+                .then(datos => {
+                    // Y luego copio ese texto en HTML_a_mostrar
+                    this.HTML_a_mostrar +=
+                        `<div class="tarjeta">
+                 <img src = "${datos.results[0].picture.large}"</img><br>
+                 Nombre: ${datos.results[0].name.last}, ${datos.results[0].name.first}<br>
+                 User: ${datos.results[0].login.username}<br>
+                 ${datos.results[0].email} <br>
+                 Reseña: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatem nihil <br>
+                  harum cum reprehenderit nulla consequuntur quisquam totae. Excepturi.
+                 </div>`
+                 this.registros++
+                })
+        }
+    }
+
+})
+miAplicacion.mount("#reseñas")
